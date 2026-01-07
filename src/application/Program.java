04ff6a9bb5867235580b4c977fac7a223e53c1d1
Arthur Barbosa <arthurbarbosaabab261@gmail.com>
2026-01-06 23:33:31 -0300

    Handling exceptions and learning how to learn the screen

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,30 +1,44 @@
 package application;
 
-import boardgame.BoardException;
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
         Scanner in =  new Scanner(System.in);
 
-            ChessMatch chessMatch = new ChessMatch();
-            while(true) {
-                UI.printBoard(chessMatch.getPieces());
-                System.out.println();
-                System.out.print("Source: ");
-                ChessPosition source = UI.readChessPosition(in);
 
-                System.out.println();
-                System.out.print("Target: ");
-                ChessPosition target = UI.readChessPosition(in);
+        ChessMatch chessMatch = new ChessMatch();
 
-                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            while (true) {
+                try {
+                    UI.clearScreen();
+                    UI.printBoard(chessMatch.getPieces());
+                    System.out.print("\n\nSource: ");
+                    ChessPosition source = UI.readChessPosition(in);
+
+                    System.out.println();
+                    System.out.print("Target: ");
+                    ChessPosition target = UI.readChessPosition(in);
+
+                    ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                }
+                catch (ChessException | InputMismatchException e){
+                    System.out.println("\n" + e.getMessage());
+                    System.out.println("Press 'Enter' to try again...");
+                    in.nextLine();
+                }
+                /*catch (InputMismatchException e){
+                    System.out.println(e.getMessage());
+                    in.nextLine();
+                }*/
+
             }
 
     }
-
 }
--- src/application/UI.java
+++ src/application/UI.java
@@ -3,7 +3,6 @@ package application;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
-
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -28,6 +27,13 @@ public class UI {
     public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
     public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+
+    // https://stackoverflow.com/questions/2979383/java-clear-the-console
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
 
     public static ChessPosition readChessPosition (Scanner in){
         try {
